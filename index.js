28f52e8e0101fdb87080f11b3766549afe3f08f3
Kaito Udagawa <umireon@gmail.com>
2016-12-02 16:38:28 +0900

    refactor: add stub sections for all editorconfig settings

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-var editorconfig = require('editorconfig-jxa')
+var editorconfig
+try {
+    editorconfig = require('editorconfig-jxa')
+} catch (e) {
+    editorconfig = Library('editorconfig-jxa')
+}
+
 var app = new Application('CotEditor')
 var doc = app.documents[0]
 var file = doc.file()
 if (file) {
   var config = editorconfig.parseSync(file.toString())
-  // config.indent_style
-  // config.indent_size
-  if (config.tab_width) doc.tabWidth = config.indent_size
-  if (config.end_of_line) doc.lineEnding = config.end_of_line.toUpperCase()
-  // config.charset
-  // config.trim_trailing_whitespace
-  // config.insert_final_newline
+
+  if ('indent_style' in config) {
+    // TODO: No way to implement
+  }
+
+  if ('indent_size' in config) {
+    // TODO: No way to implement
+  }
+
+  if ('tab_width' in config) {
+    doc.tabWidth = config.indent_size
+  }
+
+  if ('end_of_line' in config) {
+    doc.lineEnding = config.end_of_line.toUpperCase()
+  }
+
+  if ('charset' in config) {
+    // TODO: No way to implement
+  }
+
+  if ('trim_trailing_whitespace' in config) {
+    // TODO: No way to implement
+  }
+
+  if ('insert_final_newline' in config) {
+    // TODO: No way to implement
+  }
 }
