9b812d7c73013074ad88b79356f03391c9dc7e4e
Kaito Udagawa <umireon@gmail.com>
2016-12-02 15:20:46 +0900

    feat: support tab_width and end_of_line

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+var editorconfig = require('editorconfig-jxa')
 var app = new Application('CotEditor')
-app.includeStandardAdditions = true
-
 var doc = app.documents[0]
-doc.tabWidth = 4
+var file = doc.file()
+if (file) {
+  var config = editorconfig.parseSync(file.toString())
+  // config.indent_style
+  // config.indent_size
+  if (config.tab_width) doc.tabWidth = config.indent_size
+  if (config.end_of_line) doc.lineEnding = config.end_of_line.toUpperCase()
+  // config.charset
+  // config.trim_trailing_whitespace
+  // config.insert_final_newline
+}
